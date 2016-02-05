dd87e303b3f78a342b203c179fc83b5da51c28ab
Kayneth <dylan.temboucti@gmail.com>
2016-02-05 20:07:41 +0100

    End script in php - I begin the one in Javascript :dancer:

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Can Codsworth say your name ?</title>
 </head>
 <body>
 	<?php
-		$file = "names.txt";
-		if(isset($_POST['name'])){
-			$nameTry = $_POST['name'];
-			print_r(said($nameTry)); die();
-		}else{
-			$nameTry = "";
-		}
+		$fileName = "names.txt"; //fichier de noms
 
+
+		//récupère la liste des noms et en fait un tab
 		function getList($file){
 			$list = file_get_contents($file);
 			$listTab =  explode("\n", $list);
@@ -21,15 +17,33 @@
 			return $listTab;
 		}
 
-		function said($nameTry){
-			$names = getList($file);
+
+		//récupère le nom passé en POST et le compare à chaque case du tab
+		function said($nameTry, $fileName){
+			$names = getList($fileName);
+			$ret = false;
 
 			foreach ($names as $name) {
-				if($nameTry == $name){
-					return true;
+				if($nameTry === trim($name)){
+					$ret = true;
 				}
 			}
-			return false;
+			return $ret;
+		}
+
+
+		//si le nom est passé en paramètre
+		if(isset($_POST['name'])){
+			$nameTry = trim($_POST['name']); // on le met dans une autre var
+			if (said($nameTry, $fileName)){ //si le prénom est prononçable
+				$result = 'Le nom '. $nameTry .' peut être prononcé par Codsworth.';
+			}else{
+				$result = 'Le nom '. $nameTry .' n\'est pas prononçable';
+			}
+		}
+		else{
+			$nameTry = "";
+			$result = "";
 		}
 	?>
 
@@ -38,6 +52,8 @@
 		<input type="submit">
 	</form>
 
-	<p><?php if(isset($nameTry)){echo 'Le nom '. $nameTry .' peut être prononcé par Codsworth.';} ?></p>
+	<p><?php echo $result; ?></p>
+
+	<p>Code by Dylan 'Kayneth' Temboucti </p>
 </body>
 </html>
